48eda36213659417e2090fcc5cb1f4cb7b88eb71
Chris Wiegman <chris.wiegman@springbox.com>
2013-09-18 10:04:14 -0500

    Remove pass by reference for PHP 5.4

--- inc/admin.php
+++ inc/admin.php
@@ -1,194 +1,207 @@
 <?php
 
-if (!class_exists('mssls_admin')) {
+if ( !class_exists( 'mssls_admin' ) ) {
 
 	class mssls_admin extends bit51_mssls {
-		
+
 		/**
 		 * Initialize admin function
 		 */
 		function __construct() {
 			global $blog_id;
-			
-			if ($blog_id == 1) {
-			
+
+			if ( $blog_id == 1 ) {
+
 				//add scripts and css
-				add_action('admin_print_scripts', array(&$this, 'config_page_scripts'));
-				add_action('admin_print_styles', array(&$this, 'config_page_styles'));
-			
+				add_action( 'admin_print_scripts', array( $this, 'config_page_scripts' ) );
+				add_action( 'admin_print_styles', array( $this, 'config_page_styles' ) );
+
 				//add menu items
-				add_action('admin_menu', array(&$this, 'register_settings_page'));
-			
+				add_action( 'admin_menu', array( $this, 'register_settings_page' ) );
+
 				//add settings
-				add_action('admin_init', array(&$this, 'register_settings'));
-			
+				add_action( 'admin_init', array( $this, 'register_settings' ) );
+
 				//add action link
-				add_filter('plugin_action_links', array(&$this, 'add_action_link'), 10, 2);
-			
+				add_filter( 'plugin_action_links', array( $this, 'add_action_link' ), 10, 2 );
+
 				//add donation reminder
-				add_action('admin_init', array(&$this, 'ask'));
-			
+				add_action( 'admin_init', array( $this, 'ask' ) );
+
 			}
 		}
-	
+
 		/**
 		 * Register page settings
 		 */
 		function register_settings_page() {
-			add_options_page(__($this->pluginname, $this->hook), __($this->pluginname, $this->hook), $this->accesslvl, $this->hook, array(&$this,'mssls_admin_init'));
-		}	
-		
+			add_options_page( __( $this->pluginname, $this->hook ), __( $this->pluginname, $this->hook ), $this->accesslvl, $this->hook, array( $this, 'mssls_admin_init' ) );
+		}
+
 		/**
 		 * Register admin page main content
 		 * To add more boxes to the admin page add a 2nd inner array item with title and callback function or content
 		 */
 		function mssls_admin_init() {
-			$this->admin_page($this->pluginname . ' ' . __('Options', $this->hook), 
+			$this->admin_page( $this->pluginname . ' ' . __( 'Options', $this->hook ),
 				array(
-					array(__('Instructions', $this->hook), 'install_instructions'), //primary admin page content
-					array(__('General Options', $this->hook), 'general_options'), //primary admin page content
-					array(__('Exclude Sites', $this->hook), 'exclude_options') //choose sites to be excluded from view
+					array( __( 'Instructions', $this->hook ), 'install_instructions' ), //primary admin page content
+					array( __( 'General Options', $this->hook ), 'general_options' ), //primary admin page content
+					array( __( 'Exclude Sites', $this->hook ), 'exclude_options' ) //choose sites to be excluded from view
 				)
 			);
 		}
-		
+
 		/**
 		 * Create instructions block
 		 */
 		function install_instructions() {
 			?>
-			<p><?php echo __('Set your options below and then enter the shortcode', $this->hook) . ' <strong><em>[site-list]</em></strong> ' . __('where you would like your site list to appear (you can even enter it in a text widget).', $this->hook); ?></p>
-			<p><?php echo __('You can overwrite the settings below to make it easier to use the shortcode in multiple locations. Here are the options:', $this->hook); ?></p>
+			<p><?php echo __( 'Set your options below and then enter the shortcode', $this->hook ) . ' <strong><em>[site-list]</em></strong> ' . __( 'where you would like your site list to appear (you can even enter it in a text widget).', $this->hook ); ?></p>
+			<p><?php echo __( 'You can overwrite the settings below to make it easier to use the shortcode in multiple locations. Here are the options:', $this->hook ); ?></p>
 			<ul>
-				<li><?php echo __('sort: alpha for alphabetically (anything else will sort by created date).', $this->hook); ?></li>
-				<li><?php echo __('limit: set a new limit (or 0 for no limit).', $this->hook); ?></li>
-				<li><?php echo __('newwin: 1 to open links in a new window, 0 to open them in a current window.', $this->hook); ?></li>
-				<li><?php echo __('showtag: 1 to show site description (tagline), 0 to hide it.', $this->hook); ?></li>
+				<li><?php echo __( 'sort: alpha for alphabetically (anything else will sort by created date).', $this->hook ); ?></li>
+				<li><?php echo __( 'limit: set a new limit (or 0 for no limit).', $this->hook ); ?></li>
+				<li><?php echo __( 'newwin: 1 to open links in a new window, 0 to open them in a current window.', $this->hook ); ?></li>
+				<li><?php echo __( 'showtag: 1 to show site description (tagline), 0 to hide it.', $this->hook ); ?></li>
 			</ul>
-			<p><?php echo __('example: to sort alphabetically and show site descriptions you can overwrite the default options below using the following tag', $this->hook); ?><br /><em><strong>[site-list sort=alpha showtag=1]</strong></em></p>
-			<?php
+			<p><?php echo __( 'example: to sort alphabetically and show site descriptions you can overwrite the default options below using the following tag', $this->hook ); ?>
+				<br/><em><strong>[site-list sort=alpha showtag=1]</strong></em></p>
+		<?php
 		}
-		
+
 		/**
 		 * Create admin page main content
 		 */
 		function general_options() {
 			?>
 			<form method="post" action="options.php">
-			<?php settings_fields('bit51_mssls_options'); //use main settings group ?>
-			<?php $options = get_option($this->primarysettings); //use settings fields ?>
+				<?php settings_fields( 'bit51_mssls_options' ); //use main settings group ?>
+				<?php $options = get_option( $this->primarysettings ); //use settings fields ?>
 				<table class="form-table">
 					<tr valign="top">
 						<th scope="row">
-							<label for "sortby"><?php _e('Sort By', $this->hook); ?></label>
+							<label for "sortby"><?php _e( 'Sort By', $this->hook ); ?></label>
 						</th>
 						<td>
-							<input name="bit51_mssls[sortby]" id="sortby" value="0" type="radio" <?php checked('0', $options['sortby']); ?> /> <?php _e('Alphabetically', $this->hook); ?><br />
-							<input name="bit51_mssls[sortby]" id="sortby" value="1" type="radio" <?php checked('1', $options['sortby']); ?> /> <?php _e('Site Creation Date', $this->hook); ?>
+							<input name="bit51_mssls[sortby]" id="sortby" value="0"
+							       type="radio" <?php checked( '0', $options['sortby'] ); ?> /> <?php _e( 'Alphabetically', $this->hook ); ?>
+							<br/>
+							<input name="bit51_mssls[sortby]" id="sortby" value="1"
+							       type="radio" <?php checked( '1', $options['sortby'] ); ?> /> <?php _e( 'Site Creation Date', $this->hook ); ?>
 						</td>
 					</tr>
 					<tr valign="top">
 						<th scope="row">
-							<label for "showtag"><?php _e('Show Taglines', $this->hook); ?></label>
+							<label for "showtag"><?php _e( 'Show Taglines', $this->hook ); ?></label>
 						</th>
 						<td>
-							<input name="bit51_mssls[showtag]" id="sortby" value="0" type="radio" <?php checked('0', $options['showtag']); ?> /> <?php _e('No', $this->hook); ?><br />
-							<input name="bit51_mssls[showtag]" id="sortby" value="1" type="radio" <?php checked('1', $options['showtag']); ?> /> <?php _e('Yes', $this->hook); ?>
+							<input name="bit51_mssls[showtag]" id="sortby" value="0"
+							       type="radio" <?php checked( '0', $options['showtag'] ); ?> /> <?php _e( 'No', $this->hook ); ?>
+							<br/>
+							<input name="bit51_mssls[showtag]" id="sortby" value="1"
+							       type="radio" <?php checked( '1', $options['showtag'] ); ?> /> <?php _e( 'Yes', $this->hook ); ?>
 						</td>
 					</tr>
 					<tr valign="top">
 						<th scope="row">
-							<label for "openin"><?php _e('Link Target', $this->hook); ?></label>
+							<label for "openin"><?php _e( 'Link Target', $this->hook ); ?></label>
 						</th>
 						<td>
-							<input name="bit51_mssls[openin]" id="openin" value="0" type="radio" <?php checked('0', $options['openin']); ?> /> <?php _e('Same Window', $this->hook); ?><br />
-							<input name="bit51_mssls[openin]" id="openin" value="1" type="radio" <?php checked('1', $options['openin']); ?> /> <?php _e('New Window', $this->hook); ?>
+							<input name="bit51_mssls[openin]" id="openin" value="0"
+							       type="radio" <?php checked( '0', $options['openin'] ); ?> /> <?php _e( 'Same Window', $this->hook ); ?>
+							<br/>
+							<input name="bit51_mssls[openin]" id="openin" value="1"
+							       type="radio" <?php checked( '1', $options['openin'] ); ?> /> <?php _e( 'New Window', $this->hook ); ?>
 						</td>
 					</tr>
 					<tr valign="top">
 						<th scope="row">
-							<label for "limit"><?php _e('Limit Output', $this->hook); ?></label>
+							<label for "limit"><?php _e( 'Limit Output', $this->hook ); ?></label>
 						</th>
 						<td>
-							<input name="bit51_mssls[limit]" id="limit" value="<?php echo $options['limit']; ?>" type="text" /> <br />
-							<p><?php _e('Limit the number of sites displayed. This is useful if using the shortcode in a text widget. Set 0 for no limit.', $this->hook); ?></p>
+							<input name="bit51_mssls[limit]" id="limit" value="<?php echo $options['limit']; ?>"
+							       type="text"/> <br/>
+
+							<p><?php _e( 'Limit the number of sites displayed. This is useful if using the shortcode in a text widget. Set 0 for no limit.', $this->hook ); ?></p>
 						</td>
 					</tr>
 					<tr valign="top">
 						<th scope="row">
-							<label for "openin"><?php _e('Exclude Sites', $this->hook); ?></label>
+							<label for "openin"><?php _e( 'Exclude Sites', $this->hook ); ?></label>
 						</th>
 						<td>
 							<?php
-								global $wpdb;
-								global $table_prefix;
-								$options = get_option($this->primarysettings);
-								
-								//get list of all blogs not marked mature, archived, or spam
-								$blogs = $wpdb->get_col("SELECT blog_id FROM `" . $wpdb->blogs . "` WHERE public = '1' AND archived = '0' AND mature = '0' AND spam = '0' ORDER BY blog_id DESC");
-								
-								//make sure there are still blogs left
-								if ($blogs) {
-									
-									foreach ($blogs as $blog) {
-										if ($blog == '1') { //set correct table for primary blog
-											$table =  $wpdb->base_prefix . 'options';
-										} else {
-											$table = $wpdb->base_prefix . $blog . '_options';
-										}
-										
-										if (is_array($options['excluded'])) {//get array of excluded blog id's
-											$excluded = $options['excluded']; 
-										} else {
-											$excluded = unserialize($options['excluded']);
-										}
-										
-										if (is_array($excluded) && in_array($blog, $excluded)) { //to check or not to check
-											$checked = "checked";
-										} else {
-											$checked = "";
-										}
-											
-										$sitedetails = $wpdb->get_results('SELECT option_value FROM `' . $table . '` WHERE option_name IN (\'siteurl\',\'blogname\') ORDER BY option_name DESC'); //get site details
-										
-										if($sitedetails) { //as long as blog exists display it with a checkbox
-											echo '<input type="checkbox" ' . $checked . ' name="bit51_mssls[excluded' . $blog . ']" value="' . $blog . '" id="' . $blog . '" /> <a href="' . $sitedetails[0]->option_value . '">' . $sitedetails[1]->option_value . '</a><br />';
-										}
+							global $wpdb;
+							global $table_prefix;
+							$options = get_option( $this->primarysettings );
+
+							//get list of all blogs not marked mature, archived, or spam
+							$blogs = $wpdb->get_col( "SELECT blog_id FROM `" . $wpdb->blogs . "` WHERE public = '1' AND archived = '0' AND mature = '0' AND spam = '0' ORDER BY blog_id DESC" );
+
+							//make sure there are still blogs left
+							if ( $blogs ) {
+
+								foreach ( $blogs as $blog ) {
+									if ( $blog == '1' ) { //set correct table for primary blog
+										$table = $wpdb->base_prefix . 'options';
+									} else {
+										$table = $wpdb->base_prefix . $blog . '_options';
+									}
+
+									if ( is_array( $options['excluded'] ) ) { //get array of excluded blog id's
+										$excluded = $options['excluded'];
+									} else {
+										$excluded = unserialize( $options['excluded'] );
+									}
+
+									if ( is_array( $excluded ) && in_array( $blog, $excluded ) ) { //to check or not to check
+										$checked = "checked";
+									} else {
+										$checked = "";
+									}
+
+									$sitedetails = $wpdb->get_results( 'SELECT option_value FROM `' . $table . '` WHERE option_name IN (\'siteurl\',\'blogname\') ORDER BY option_name DESC' ); //get site details
+
+									if ( $sitedetails ) { //as long as blog exists display it with a checkbox
+										echo '<input type="checkbox" ' . $checked . ' name="bit51_mssls[excluded' . $blog . ']" value="' . $blog . '" id="' . $blog . '" /> <a href="' . $sitedetails[0]->option_value . '">' . $sitedetails[1]->option_value . '</a><br />';
 									}
 								}
+							}
 							?>
-							<p><?php _e('Put a checkmark below next to the sites you would like to remove from your site list.', &$this->hook); ?></p>
+							<p><?php _e( 'Put a checkmark below next to the sites you would like to remove from your site list.', $this->hook ); ?></p>
 						</td>
 					</tr>
 				</table>
-				<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
+				<p class="submit"><input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>"/>
+				</p>
 			</form>
-			<?php
+		<?php
 		}
-		
+
 		/**
 		 * Validate input
 		 */
-		function mssls_val_options($input) {
+		function mssls_val_options( $input ) {
 			$exclude = array(); //create exclude array
-			
+
 			//if we're dealing with one of the exclude fields add the value to the exclude array
-		    foreach ($input as $field => $value) {
-		    	if ($field != 'sortby' && $field != 'openin' && $field != 'showtag' && $field != 'limit') {
-		    		unset($input[$field]);
-		    		$exclude[] = $value;
-		    	}
-		    }
-		    
-		    $input['excluded'] = serialize($exclude); //convert array to string and add to $input
-		    
-		    //process non-exclude fields
-		    $input['sortby'] = ($input['sortby'] == 1 ? 1 : 0);
-		    $input['openin'] = ($input['openin'] == 1 ? 1 : 0);
-		    $input['showtag'] = ($input['showtag'] == 1 ? 1 : 0);
-		    $input['limit'] = intval($input['limit']);
-		    
-		    return $input;
+			foreach ( $input as $field => $value ) {
+				if ( $field != 'sortby' && $field != 'openin' && $field != 'showtag' && $field != 'limit' ) {
+					unset( $input[$field] );
+					$exclude[] = $value;
+				}
+			}
+
+			$input['excluded'] = serialize( $exclude ); //convert array to string and add to $input
+
+			//process non-exclude fields
+			$input['sortby'] = ( $input['sortby'] == 1 ? 1 : 0 );
+			$input['openin'] = ( $input['openin'] == 1 ? 1 : 0 );
+			$input['showtag'] = ( $input['showtag'] == 1 ? 1 : 0 );
+			$input['limit'] = intval( $input['limit'] );
+
+			return $input;
 		}
 	}
 }
--- multi-site-site-list-shortcode.php
+++ multi-site-site-list-shortcode.php
@@ -3,7 +3,7 @@
 	Plugin Name: Multi-Site Site List Shortcode
 	Plugin URI: http://bit51.com/software/multi-site-site-list-shortcode/
 	Description: Displays the site list with a shortcode for network installations.
-	Version: 5.4.1
+	Version: Dev
 	Domain Path: /languages
 	Author: Bit51
 	Author URI: http://bit51.com
@@ -12,106 +12,106 @@
 */
 
 //load the text domain
-load_plugin_textdomain('multi_site_site_list_shortcode', false, dirname(plugin_basename( __FILE__ )) . '/languages');
+load_plugin_textdomain( 'multi_site_site_list_shortcode', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 
 //Require common Bit51 library
-require_once(plugin_dir_path(__FILE__) . 'lib/bit51/bit51.php');
+require_once( plugin_dir_path( __FILE__ ) . 'lib/bit51/bit51.php' );
 
-if (!class_exists('bit51_mssls')) {
+if ( !class_exists( 'bit51_mssls' ) ) {
 
 	class bit51_mssls extends Bit51 {
-	
-		public $pluginversion 	= '5.4.1'; //current plugin version
-	
+
+		public $pluginversion = '5.4.1'; //current plugin version
+
 		//important plugin information
-		public $hook 			= 'multi_site_site_list_shortcode';
-		public $pluginbase		= 'multi-site-site-list-shortcode/multi-site-site-list-shortcode.php';
-		public $pluginname		= 'Multi-Site Site List Shortcode';
-		public $homepage		= 'http://bit51.com/software/multi-site-site-list-shortcode/';
-		public $supportpage 	= 'http://wordpress.org/support/plugin/multi-site-site-list-shortcode';
-		public $wppage 			= 'http://wordpress.org/extend/plugins/multi-site-site-list-shortcode/';
-		public $accesslvl		= 'manage_network_options';
-		public $paypalcode		= '7GDQDFENDBDAA';
-		public $plugindata 		= 'bit51_mssls_data';
-		public $primarysettings	= 'bit51_mssls';
-		public $settings		= array(
-			'bit51_mssls_options' 	=> array(
-				'bit51_mssls' 			=> array(
-					'callback' 				=> 'mssls_val_options',
-					'sortby' 				=> '0',
-					'openin' 				=> '0',
-					'limit' 				=> '0',
-					'showtag' 				=> '0',
-					'excluded' 				=> array()
+		public $hook = 'multi_site_site_list_shortcode';
+		public $pluginbase = 'multi-site-site-list-shortcode/multi-site-site-list-shortcode.php';
+		public $pluginname = 'Multi-Site Site List Shortcode';
+		public $homepage = 'http://bit51.com/software/multi-site-site-list-shortcode/';
+		public $supportpage = 'http://wordpress.org/support/plugin/multi-site-site-list-shortcode';
+		public $wppage = 'http://wordpress.org/extend/plugins/multi-site-site-list-shortcode/';
+		public $accesslvl = 'manage_network_options';
+		public $paypalcode = '7GDQDFENDBDAA';
+		public $plugindata = 'bit51_mssls_data';
+		public $primarysettings = 'bit51_mssls';
+		public $settings = array(
+			'bit51_mssls_options' => array(
+				'bit51_mssls' => array(
+					'callback' => 'mssls_val_options',
+					'sortby' => '0',
+					'openin' => '0',
+					'limit' => '0',
+					'showtag' => '0',
+					'excluded' => array()
 				)
 			)
 		);
 
 		/**
-		 * Register the shortcode in instantiation 
+		 * Register the shortcode in instantiation
 		 */
 		function __construct() {
-		
+
 			//set path information
-			define('MSSLS_PP', plugin_dir_path(__FILE__));
-			define('MSSLS_PU', plugin_dir_url(__FILE__));
-		
+			define( 'MSSLS_PP', plugin_dir_path( __FILE__ ) );
+			define( 'MSSLS_PU', plugin_dir_url( __FILE__ ) );
+
 			//require admin page
-			require_once(plugin_dir_path(__FILE__) . 'inc/admin.php');
-			new mssls_admin($this);
-			
+			require_once( plugin_dir_path( __FILE__ ) . 'inc/admin.php' );
+			new mssls_admin( $this );
+
 			//require setup information
-			require_once(plugin_dir_path(__FILE__) . 'inc/setup.php');
-			register_activation_hook( __FILE__, array('mssls_setup', 'on_activate'));
-			register_deactivation_hook( __FILE__, array('mssls_setup', 'on_deactivate'));
-			register_uninstall_hook( __FILE__, array('mssls_setup', 'on_uninstall'));
-			
-			add_shortcode('site-list', array(&$this, 'display_site_list'));
-			if (!is_admin()) {
-				add_filter('widget_text', 'do_shortcode');
+			require_once( plugin_dir_path( __FILE__ ) . 'inc/setup.php' );
+			register_activation_hook( __FILE__, array( 'mssls_setup', 'on_activate' ) );
+			register_deactivation_hook( __FILE__, array( 'mssls_setup', 'on_deactivate' ) );
+			register_uninstall_hook( __FILE__, array( 'mssls_setup', 'on_uninstall' ) );
+
+			add_shortcode( 'site-list', array( $this, 'display_site_list' ) );
+			if ( !is_admin() ) {
+				add_filter( 'widget_text', 'do_shortcode' );
 			}
 		}
-		
+
 		/**
 		 * Create site list
 		 */
-		function display_site_list($attr) {
+		function display_site_list( $attr ) {
 			global $wpdb;
 			global $table_prefix;
 			$output = '';
-			
+
 			//this is a workaround for lack of settings api with multi-site
-			$options = $wpdb->get_results('SELECT option_value FROM `' . $wpdb->base_prefix . 'options` WHERE option_name IN (\'' . $this->primarysettings . '\') ORDER BY option_name DESC');
-			
+			$options = $wpdb->get_results( 'SELECT option_value FROM `' . $wpdb->base_prefix . 'options` WHERE option_name IN (\'' . $this->primarysettings . '\') ORDER BY option_name DESC' );
+
 			//convert options to array so we can use it like normal
-			$options = unserialize($options[0]->option_value);
-			
+			$options = unserialize( $options[0]->option_value );
+
 			//get blog list
-			$blogs = $wpdb->get_col("SELECT blog_id FROM `" . $wpdb->blogs . "` WHERE public = '1' AND archived = '0' AND mature = '0' AND spam = '0' ORDER BY blog_id DESC");
-			
+			$blogs = $wpdb->get_col( "SELECT blog_id FROM `" . $wpdb->blogs . "` WHERE public = '1' AND archived = '0' AND mature = '0' AND spam = '0' ORDER BY blog_id DESC" );
+
 			//make sure there are blogs
-			if ($blogs) {
-			
+			if ( $blogs ) {
+
 				$output .= '<div id="mssls"><ul>'; //assign a div to make styling easier
 				$siteArray = array(); //initial array of sites to display
-				
-				foreach ($blogs as $blog) {
-					if ($blog == '1') { //get blog options from the right table (blog 1 needs a special tablename)
-						$table =  $wpdb->base_prefix . 'options';
+
+				foreach ( $blogs as $blog ) {
+					if ( $blog == '1' ) { //get blog options from the right table (blog 1 needs a special tablename)
+						$table = $wpdb->base_prefix . 'options';
 					} else {
 						$table = $wpdb->base_prefix . $blog . '_options';
 					}
-					
-					if (is_array($options['excluded'])) {//get array of excluded blog id's
-						$excluded = $options['excluded']; 
+
+					if ( is_array( $options['excluded'] ) ) { //get array of excluded blog id's
+						$excluded = $options['excluded'];
 					} else {
-						$excluded = unserialize($options['excluded']);
+						$excluded = unserialize( $options['excluded'] );
 					}
-						
-					$sitedetails = $wpdb->get_results('SELECT option_value FROM `' . $table . '` WHERE option_name IN (\'siteurl\',\'blogname\',\'blogdescription\') ORDER BY option_name DESC'); //get blog details
-					
-					if (isset($attr['sort'])) {
-						if (strtolower($attr['sort']) == 'alpha') {
+
+					$sitedetails = $wpdb->get_results( 'SELECT option_value FROM `' . $table . '` WHERE option_name IN (\'siteurl\',\'blogname\',\'blogdescription\') ORDER BY option_name DESC' ); //get blog details
+
+					if ( isset( $attr['sort'] ) ) {
+						if ( strtolower( $attr['sort'] ) == 'alpha' ) {
 							$sortby = 0;
 						} else {
 							$sortby = 1;
@@ -119,9 +119,9 @@ if (!class_exists('bit51_mssls')) {
 					} else {
 						$sortby = $options['sortby'];
 					}
-					
-					if($sitedetails && !in_array($blog, $excluded)) { //if the blog exists and isn't on the exclusion list add it to array
-						if ($sortby == 0) { //proper array construction depending on sort
+
+					if ( $sitedetails && !in_array( $blog, $excluded ) ) { //if the blog exists and isn't on the exclusion list add it to array
+						if ( $sortby == 0 ) { //proper array construction depending on sort
 							$siteArray[$sitedetails[1]->option_value]['url'] = $sitedetails[0]->option_value;
 							$siteArray[$sitedetails[1]->option_value]['id'] = $blog;
 							$siteArray[$sitedetails[1]->option_value]['desc'] = $sitedetails[2]->option_value;
@@ -132,65 +132,65 @@ if (!class_exists('bit51_mssls')) {
 						}
 					}
 				}
-				
-				ksort($siteArray); //sort array
-				
+
+				ksort( $siteArray ); //sort array
+
 			}
-			
+
 			//create link target if necessary
-			
-			if (isset($attr['newwin'])) {
+
+			if ( isset( $attr['newwin'] ) ) {
 				$openin = $attr['newwin'];
 			} else {
 				$openin = $options['openin'];
 			}
-			
-			if ($openin == 1) {
+
+			if ( $openin == 1 ) {
 				$target = ' target="_blank" ';
 			} else {
 				$target = ' ';
 			}
-			
+
 			$count = 0;
-			
+
 			//add sites to output string
-			foreach ($siteArray as $site => $value) {
-			
-				if(isset($attr['limit'])) {
+			foreach ( $siteArray as $site => $value ) {
+
+				if ( isset( $attr['limit'] ) ) {
 					$limit = $attr['limit'];
 				} else {
 					$limit = $options['limit'];
 				}
-			
-				if ($count < $limit || $limit == 0) {
-				
-					if (isset($attr['showtag'])) {
+
+				if ( $count < $limit || $limit == 0 ) {
+
+					if ( isset( $attr['showtag'] ) ) {
 						$showtag = $attr['showtag'];
 					} else {
 						$showtag = $options['showtag'];
 					}
-			
-					if ($showtag == 1 && strlen($value['desc']) > 0) {
+
+					if ( $showtag == 1 && strlen( $value['desc'] ) > 0 ) {
 						$desc = '<br /><p>' . $value['desc'] . '</p>';
 					} else {
 						$desc = '';
 					}
-			
-					if ($sortby == 0) {
-						$output .= '<li><a href="' .  $value['url'] . '"' . $target  . '>' . $site . '</a>' .  $desc . '</li>';
+
+					if ( $sortby == 0 ) {
+						$output .= '<li><a href="' . $value['url'] . '"' . $target . '>' . $site . '</a>' . $desc . '</li>';
 					} else {
-						$output .= '<li><a href="' .  $value['url'] . '"' . $target  . '>' . $value['title'] . '</a>' .  $desc . '</li>';
+						$output .= '<li><a href="' . $value['url'] . '"' . $target . '>' . $value['title'] . '</a>' . $desc . '</li>';
 					}
-					
+
 					$count++;
-				
+
 				} else {
 					break;
-				}		
+				}
 			}
-			
+
 			return $output . '</ul></div>';
-			
+
 		}
 	}
 }
